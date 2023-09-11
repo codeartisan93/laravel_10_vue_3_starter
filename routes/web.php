1d30242d0cc0b53d0ebef6f5d35da7f51418395b
codeartisan93 <narutoninja815@hotmail.com>
2023-09-11 11:47:10 +0900

    api routes added

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('app');
-})
-->name('application');
+// Route::get('/', function () {
+//     return view('app');
+// })
+// ->name('application');
+
+Route::get('{any}', function () {
+    return view('layouts.app');
+})->where('any', '.*');
