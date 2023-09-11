07e2603fe9513369abd3afc2f179a7822c9fc44f
codeartisan93 <narutoninja815@hotmail.com>
2023-09-11 13:07:40 +0900

    update vue axios router usage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,19 @@
 import './bootstrap';
-import Vue from 'vue/dist/vue'
-window.Vue = require('vue');
+import { createApp } from 'vue'
  
 import App from './App.vue';
-import VueRouter from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 import {routes} from './routes';
  
-Vue.use(VueRouter);
-Vue.use(VueAxios, axios);
- 
-const router = new VueRouter({
-    mode: 'history',
+const app = createApp(App)
+
+const router = createRouter({
+    history: createWebHistory(),
     routes: routes
 });
- 
-const app = new Vue({
-    el: '#app',
-    router: router,
-    render: h => h(App),
-});
+
+app.use(router)
+app.use(VueAxios, axios)
+app.mount('#app')
